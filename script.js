0f7f06560c345cce761a981204bab74016499f51
Ritesh Kumar <aryaritesh506@gmail.com>
2025-01-27 21:04:12 +0530

    firs mpublish

---
+++ script.js
@@ -0,0 +1,18 @@
+// prompt("Enter your name and occupation");
+console.log("MY NAME IS RITESH");
+console.log("I AM A WEB DEVELOPER");
+// alert("Hello world");
+// document.body.style.backgroundColor = "lightblue";
+// let a;
+// for(a=1; a<=100; a++){
+//     console.log(a);
+// }
+let Ritesh, Rohit, Shubham, Rahul, Rakesh;
+let heros = [Ritesh, Rohit, Shubham, Rahul, Rakesh];
+for(let idx=0; idx<heros.length; idx++){
+    console.log(heros[idx]);
+}
+// let marks = [52,62,22,64,20];
+// for(marks=0;marks<=5;marks++){
+//     console.log(marks);
+// }
